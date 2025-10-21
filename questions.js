214adaa6abcf706f325104fe48b3cc8c34a9d27a
bigsmoke3654-web <bigsmoke3654@gmail.com>
2025-10-21 13:14:59 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What technological breakthrough is Zama best known for in the blockchain industry?",
+    answer: "Fully Homomorphic Encryption (FHE)",
+    options: [
+      "Zero-Knowledge Proofs",
+      "Elliptic Curve Cryptography",
+      "Fully Homomorphic Encryption (FHE)",
+      "Quantum Entanglement"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In 2025, what significant funding milestone did Zama achieve, making it the first unicorn in its space?",
+    answer: "$57 million Series B funding",
+    options: [
+      "$10 million in seed funding",
+      "$73 million Series A funding",
+      "$57 million Series B funding",
+      "$1 million government grant"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who are the main founders of Zama?",
+    answer: "Dr. Pascal Paillier and Dr. Rand Hindi",
+    options: [
+      "Vitalik Buterin and Gavin Wood",
+      "Anatoly Yakovenko and Juan Benet",
+      "Dr. Pascal Paillier and Dr. Rand Hindi",
+      "Ken Seiff and Paul Veradittakit"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What main advantage does Zama’s FHEVM provide developers over other confidentiality solutions?",
+    answer: "Allows confidential smart contracts with familiar tools like Solidity",
+    options: [
+      "Requires learning a new programming language",
+      "Allows confidential smart contracts with familiar tools like Solidity",
+      "Only works on private blockchains",
+      "Offers no composability with DeFi protocols"
+    ]
+  },
+    {
+    numb: 5,
+    question: "When is Zama’s mainnet and $ZAMA token launch expected?",
+    answer: "End of 2025",
+    options: [
+      "i Don't know gng",
+      "End of 2025",
+      "Early 2026",
+      "It already launched in 2022"
+    ]
+  },
+];
